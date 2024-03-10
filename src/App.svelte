1b086b2a3e5a8f4f834deaa3eef3db0fd65ad1ed
Luiz Moura <muniz.mourafelipe@gmail.com>
2024-03-10 12:31:25 -0300

    Add Svelte Splide to use carousel functionalities

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "@splidejs/svelte-splide/css";
+  import { Splide, SplideSlide } from "@splidejs/svelte-splide";
+</script>
+
+<Splide aria-label="My Favorite Images">
+  <SplideSlide>
+    <div>Title 1</div>
+  </SplideSlide>
+  <SplideSlide>
+    <div>Title 2</div>
+  </SplideSlide>
+</Splide>
